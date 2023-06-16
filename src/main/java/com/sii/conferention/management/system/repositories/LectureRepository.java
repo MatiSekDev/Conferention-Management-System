0452acf01dc444d6f1d5c5c1de62841232c605c9
MatiSekDev <mateuszss19@interia.pl>
2023-06-16 02:14:45 +0200

    user can see what lectures he joined
    new GET methid in UserController used for letting user know what lectures he have joined, new Dto: UserLecturesDto

--- src/main/java/com/sii/conferention/management/system/repositories/LectureRepository.java
+++ src/main/java/com/sii/conferention/management/system/repositories/LectureRepository.java
@@ -1,15 +1,11 @@
 package com.sii.conferention.management.system.repositories;
 
 import com.sii.conferention.management.system.entities.LectureEntity;
-import com.sii.conferention.management.system.entities.RoleEntity;
-import com.sii.conferention.management.system.enums.RoleEnum;
-import com.sii.conferention.management.system.enums.TopicTypeEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,6 +15,4 @@ public interface LectureRepository extends JpaRepository<LectureEntity, Long> {
 
     @Query("SELECT lecture FROM LectureEntity lecture WHERE lecture.id in :lecturesIdsUserIsAssignedTo")
     List<LectureEntity> findLecturesByLecturesId(List<Long> lecturesIdsUserIsAssignedTo);
-
-
 }
